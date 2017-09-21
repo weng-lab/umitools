80befa40eb86eba119a44140c5228a0e40d3fb60
yfu <yfu@yfu.me>
2017-09-21 16:58:05 -0400

    umi_graph.py

--- umitools/umi_graph.py
+++ umitools/umi_graph.py
@@ -25,14 +25,16 @@ class UmiGraph:
     def number_true_umi(self):
         '''Returns the number of UMIs after collapsing small nodes
 '''
-        print(nx.algorithms.components.weakly_connected_components(self.UG))
+        return nx.algorithms.components.number_weakly_connected_components(self.UG)
 
     def get_umi_clusters(self):
+        """Prints the number of clusters and return the main UMI for each cluster
         """
-        """
-        for c in nx.algorithms.components.weakly_connected_components(self.UG):
-            print(c)
-        
+        ret = []
+        for comp in nx.algorithms.components.weakly_connected_components(self.UG):
+            ret.append([(u, self.get_count(u)) for u in comp])
+        return ret
+            
     def print_graph(self):
         for i in self.UG.nodes:
             print("Edge(s) from {}".format(i))
@@ -44,24 +46,30 @@ class UmiGraph:
     def get_count(self, u):
         return self.UG.nodes[u]["count"]
 
-    def simplify_graph(self):
-        '''It simplfies the graph by collapsing small nodes into large nodes
-        when they are connected by edges. It finishes when there are no edges
-        left in the graph.
-'''
-        # This can be done with number_connected_components()
-        pass
-        while len(self.UG.edges) > 0:
-            # Strategy: find the smallest terminal node, collapse it into its neighbor
-            pass
+#     def simplify_graph(self):
+#         '''It simplfies the graph by collapsing small nodes into large nodes
+#         when they are connected by edges. It finishes when there are no edges
+#         left in the graph.
+# '''
+#         # This can be done with number_connected_components()
+#         pass
+#         while len(self.UG.edges) > 0:
+#             # Strategy: find the smallest terminal node, collapse it into its neighbor
+#             pass
 
     
 def main():
+    
     uc = {"AAAA": 1, "AAAC": 2, "AAAG": 1, "AAAT": 10,
           "GGGG": 100, "GGGA": 99, "GGGC": 50, "GGGT": 2}
     G = UmiGraph(uc)
-    G.print_graph()
-    G.get_umi_clusters()
+    print("Number of true UMIs for this locus: {}".format(G.number_true_umi()))
+    clusters = G.get_umi_clusters()
+    for c in clusters:
+        print(c)
     
 if __name__ == "__main__":
     main()
+
+
+    
